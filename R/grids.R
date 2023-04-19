09a0c9f4ef8c58153f36e2dd1b83493f49d8d1af
ManuelSpinola <mspinola10@gmail.com>
2023-04-19 14:33:00 -0600

    4th commit

--- R/grids.R
+++ R/grids.R
@@ -10,9 +10,11 @@
 #' @description
 #' Spatial grids for Costa Rica of 1km, 2km, 4km and 8km.
 #'
-#' #' @usage
+#' @usage
 #' cr_grid_1km
 #' cr_grid_2km
 #' cr_grid_4km
 #' cr_grid_8km
 #' @format An sf object (EPSG:5367)
+#'
+"cr_grid_1km"
